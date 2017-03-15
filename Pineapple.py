787dbb0a39ec2aae210335a52df50d2db5665fe1
matt <matthewberryhill@gmail.com>
2017-03-14 19:41:48 -0600

    implemented the ability to debug

--- Pineapple.py
+++ Pineapple.py
@@ -8,7 +8,6 @@ import Live
 
 transport_play_button = None
 transport_stop_button = None
-# transport_record_button = None
 session_box_button_count = 0
 session_box_count = 0
 
@@ -17,12 +16,21 @@ from _Framework.SessionComponent import SessionComponent
 from _Framework.ButtonElement import ButtonElement
 from _Framework.TransportComponent import TransportComponent
 
+
 class Pineapple(ControlSurface):
     def __init__(self, c_instance):
         ControlSurface.__init__(self, c_instance)
+        self.setupSessionBox()
+        self.setupControllerr()
+
+
+    def setupSessionBox(self):
         with self.component_guard():
-            self._make_transport()
-            self._make_session_box(16, 16)
+            self.session = SessionComponent(4, 4)
+            self.sessionn = SessionComponent(2, 2)
+            self.set_highlighting_session_component(self.session)
+            self.set_highlighting_session_component(self.sessionn)
+
 
         # VVV would like the try to see if the following code works VVV
 
@@ -36,17 +44,25 @@ class Pineapple(ControlSurface):
 
         # client.send(OSCMessage("/hello"))
 
-
-    def _configure_hardware_functions(self):
-        pass
-
-
-    def _request_hardware_functions(self):
-        pass
+    def log(self, message):
+        sys.stderr.write("LOG: " + message.encode("utf-8"))
 
 
-    def _handle_hardware_function_calls(self):
-        pass
+    def handle_sysex(self, midi_bytes):
+        for k in range(midi_bytes):
+            self.log(midi_bytes[k])
+
+
+    # def _configure_hardware_functions(self):
+    #     pass
+    #
+    #
+    # def _request_hardware_functions(self):
+    #     pass
+    #
+    #
+    # def _handle_hardware_function_calls(self):
+    #     pass
 
 
     def _make_transport(self):
@@ -54,26 +70,25 @@ class Pineapple(ControlSurface):
         transport_play_button = ButtonElement(False, 0, 10, 0)
         global transport_stop_button
         transport_stop_button = ButtonElement(False, 0, 10, 1)
-
         transport = TransportComponent()
         transport.set_play_button(transport_play_button)
         transport.set_stop_button(transport_stop_button)
-        # transport.set_record_button(transport_record_button)
 
 
     def _make_session_box(self, scenes, tracks):
-        self.session = SessionComponent(scenes, tracks)
-        self.session.set_offsets(0, 0)
-        for k in range(session_box_button_count, session_box_button_count +
-                (scenes * tracks)):
-            self.session.scene(k - session_box_button_count % scenes)\
-                .clip_slot(k - session_box_button_count % tracks).\
-                set_launch_button(ButtonElement(False, 0, 8, k))
-
-        self.set_highlighting_session_component(self.session)
-
-        global session_box_button_count
-        session_box_button_count = session_box_button_count + \
-                                   (scenes * tracks)
-        global session_box_count
-        session_box_count = session_box_count + 1
+        # self.session = SessionComponent(scenes, tracks)
+        # self.session.set_offsets(0, 0)
+        # for k in range(session_box_button_count, session_box_button_count +\
+        #         (scenes * tracks)):
+        #     self.session.scene(k - session_box_button_count % scenes)\
+        #         .clip_slot(k - session_box_button_count % tracks).\
+        #         set_launch_button(ButtonElement(False, 0, 8, k))
+        #
+        # self.set_highlighting_session_component(self.session)
+        #
+        # global session_box_button_count
+        # session_box_button_count = session_box_button_count + \
+        #                            (scenes * tracks)
+        # global session_box_count
+        # session_box_count = session_box_count + 1
+        pass
